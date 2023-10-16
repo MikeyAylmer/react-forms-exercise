bb8af1a0187e7f33509d79db99e9b32c46d74bdd
Michael Aylmer <michaelaylmer@Michaels-MacBook-Air.local>
2023-10-16 12:06:43 -0400

    box form and todo form exercise.

---
+++ src/Box.js
@@ -0,0 +1,12 @@
+import './Box.css'
+
+function Box({ width, height, backgroundColor }) {
+    return (
+        <div style={{ width, height, backgroundColor }}>
+            {/* Remove button */}
+        </div>
+    );
+}
+
+
+export default Box;
